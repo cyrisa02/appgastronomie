5cf07b55570a55e7ce4f3675fcfae6583738b725
cyrisa02 <cyril.gourdon.02@gmail.com>
2021-11-01 09:26:11 +0100

    Updates

---
+++ precache-manifest.e0cda2d592f4a0e65cde457ea856a075.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb1b04768b8437441bfd3cc40d37e1c",
+    "url": "/appgastronomie/index.html"
+  },
+  {
+    "revision": "72e342dc4cb2993f80d2",
+    "url": "/appgastronomie/static/css/main.51adad21.chunk.css"
+  },
+  {
+    "revision": "9eacaa3167d104211b7d",
+    "url": "/appgastronomie/static/js/2.a2461b66.chunk.js"
+  },
+  {
+    "revision": "eb3abb7fd2745f475c9f76f5b28765b9",
+    "url": "/appgastronomie/static/js/2.a2461b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e342dc4cb2993f80d2",
+    "url": "/appgastronomie/static/js/main.ce5a36a0.chunk.js"
+  },
+  {
+    "revision": "f757a66962676c6d8813",
+    "url": "/appgastronomie/static/js/runtime-main.4a3062fb.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/appgastronomie/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/appgastronomie/static/media/Questrial-Regular.37c8a1aa.ttf"
+  },
+  {
+    "revision": "a404c3657888c94e068116110f8b2c12",
+    "url": "/appgastronomie/static/media/citron.a404c365.jpeg"
+  },
+  {
+    "revision": "cd95f39b2d8f06b70b3c622147d12226",
+    "url": "/appgastronomie/static/media/pomme.cd95f39b.jpg"
+  },
+  {
+    "revision": "26b4742a03042ef6b7bc207838f7d1c8",
+    "url": "/appgastronomie/static/media/tarte.26b4742a.jpg"
+  },
+  {
+    "revision": "e109e1124dc9823091ea55325ee90e41",
+    "url": "/appgastronomie/static/media/tartec.e109e112.jpeg"
+  },
+  {
+    "revision": "cc5b8f575eee9fd428bf703ac9e77a3a",
+    "url": "/appgastronomie/static/media/tartef.cc5b8f57.jpeg"
+  }
+]);
